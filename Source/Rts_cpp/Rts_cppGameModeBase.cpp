4db0e79888bd1df91696107e30777c2daefa86a6
Krytog <danya2003.db@gmail.com>
2023-07-03 21:49:53 +0300

    Init commit

---
+++ Source/Rts_cpp/Rts_cppGameModeBase.cpp
@@ -0,0 +1,5 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+
+#include "Rts_cppGameModeBase.h"
+
---
+++ Source/Rts_cpp/Rts_cppGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Rts_cppGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RTS_CPP_API ARts_cppGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
